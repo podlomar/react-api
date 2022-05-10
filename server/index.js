c29b97aef2df69a8bfe6255b4031b04f4eccfa24
Martin Podloucký <podlouckymartin@gmail.com>
2022-05-10 20:43:30 +0200

    Simple API endpoint

--- server/index.js
+++ server/index.js
@@ -3,7 +3,12 @@ import express from 'express';
 const port = process.env.PORT ?? 2000;
 const server = express();
 
+server.use(express.json());
 server.use(express.static('dist'));
+
+server.get('/api/hello', (req, resp) => {
+  resp.send({ greet: 'hello' });
+});
 
 server.get('*', (req, resp) => {
   resp.sendFile('dist/index.html');
